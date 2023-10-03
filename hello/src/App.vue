77d578f3412d5eedbf2a3b655d1b5317fa0c90a6
Gaku Yasui <gaku.yasui@gmail.com>
2023-10-03 18:37:04 +0900

    init

---
+++ hello/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Counter1 from './components/CounterOptionsApi.vue'
+import Counter2 from './components/CounterCompositonApi.vue'
+</script>
+
+<template>
+	<div>
+		<Counter1 />
+	</div>
+	<div>
+		<Counter2 />
+	</div>
+</template>
+
+<style scoped></style>
