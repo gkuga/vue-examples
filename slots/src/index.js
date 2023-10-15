8d4ce84ecda0d96964263a7b84d41606d81fd842
Gaku Yasui <gaku.yasui@gmail.com>
2023-10-15 17:38:57 +0900

    slots: scoped slots8

--- slots/src/index.js
+++ slots/src/index.js
@@ -1,31 +1,35 @@
 window.addEventListener('load', () => {
-  const { createApp, ref } = Vue
-  const User = {
-    setup() {
-      const user = ref({
-        firstName: 'John',
-        lastName: 'Doe',
-        age: '25',
-        sex: '男性',
-      })
-      const message = ref('Hello Scoped Slot')
-      return { user, message }
-    },
-    template: `
-<span>
-  <slot :user="user" :message="message">{{ user.lastName }}</slot>
-</span>
+	const { createApp, ref } = Vue
+	const Menu = {
+		setup() {
+			const display = ref(true)
+			const onClick = () => (display.value = !display.value);
+			return { display, onClick }
+		},
+		template: `
+<div>
+	<slot name="activator" v-bind:onClick="onClick"></slot>
+  <slot v-if="display"></slot>
+</div>
 `
-  }
-  app = createApp({
-    setup() { },
-    template: `
-<User v-slot="dataFromChild">
-  <p>{{ dataFromChild }}</p>
-</User>`,
-    components: {
-      User,
-    },
-  })
-  app.mount('#app')
+	}
+	app = createApp({
+		setup() { },
+		template: `
+<Menu>
+  <template v-slot:activator="{ onClick }">
+    <button @click="onClick">Click</button>
+  </template>
+  <ul>
+    <li>List1</li>
+    <li>List2</li>
+    <li>List3</li>
+  </ul>
+</Menu>
+`,
+		components: {
+			Menu,
+		},
+	})
+	app.mount('#app')
 });
