1fe52b22cb612a112da79bf3e5a75118a542e6c5
Gaku Yasui <gaku.yasui@gmail.com>
2023-10-15 20:35:07 +0900

    slots: scoped slots 10

--- slots/src/index.js
+++ slots/src/index.js
@@ -1,51 +1,32 @@
 window.addEventListener('load', () => {
-	const { createApp, defineProps, ref } = Vue
-	const Table = {
-		props: ['header', 'items'],
-		setup() { },
+	const { onMounted, computed, createApp, defineProps, ref, useSlots } = Vue
+	const BlogPost = {
+		setup() {
+			const slots = useSlots();
+			onMounted(() => {
+				console.log(slots);
+			});
+			return { slots };
+		},
 		template: `
-<table border="1">
-  <thead>
-    <tr>
-      <th v-for="row_name in header" :key="row_name">{{ row_name }}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(item,index) in items" :key="index">
-      <td v-for="(value,index) in item" :key="index">{{ value }}</td>
-    </tr>
-  </tbody>
-</table>
+<div>
+  <h1><slot name="title">Title</slot></h1>
+  <p><slot></slot></p>
+  <p>Slot:{{ slots.title }}</p>
+</div>
 `
 	}
 	const app = createApp({
-		setup() {
-			const header = ['ID', 'FIRSTNAME', 'LASTNAME', 'EMAIL'];
-			const items = [
-				{
-					id: 1,
-					firstName: 'John',
-					lastName: 'Doe',
-					email: 'john@example.com',
-				},
-				{
-					id: 2,
-					firstName: 'Kevin',
-					lastName: 'Wood',
-					email: 'kevin@example.com',
-				},
-				{
-					id: 3,
-					firstName: 'Harry',
-					lastName: 'Bosch',
-					email: 'harry@test.com',
-				},
-			];
-			return { header, items }
-		},
-		template: `<Table :header="header" :items="items"></Table>`,
+		setup() { },
+		template: `
+<div>
+  <BlogPost>
+    <template #title>vue.js</template>
+    I'd like to know about $vm.slots.
+  </BlogPost>
+</div>`,
 		components: {
-			Table,
+			BlogPost,
 		},
 	});
 	app.mount('#app')
