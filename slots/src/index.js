34fb097bb406655813aa9a857a2753fb26e10afe
Gaku Yasui <gaku.yasui@gmail.com>
2023-10-15 17:01:11 +0900

    slots: named slots

--- slots/src/index.js
+++ slots/src/index.js
@@ -2,11 +2,22 @@ window.addEventListener('load', () => {
   const { createApp, ref } = Vue
   const User = {
     setup() {},
-    template: `<p>この人の名前は<slot></slot>です。</p>`
+    template: `
+<ul>
+  <li>名前：<slot>名無しの権兵衛</slot></li>
+  <li>年齢：<slot name="age">記入なし</slot></li>
+  <li>性別：<slot name="sex">不明</slot></li>
+</ul>
+`
   }
   app = createApp({
     setup() {},
-    template: `<User>John Doe</User>`,
+    template: `
+<User>
+  John Doe
+  <template v-slot:age>25</template>
+  <template v-slot:sex>男性</template>
+</User>`,
     components: {
       User,
     },
